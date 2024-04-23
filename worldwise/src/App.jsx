876681c322fb39a2e697d8bec888b4469f5020f1
yikliu <yikliu@umich.edu>
2024-04-23 19:39:46 -0400

    Finish the country and city components in the sidebar

--- worldwise/src/App.jsx
+++ worldwise/src/App.jsx
@@ -5,8 +5,32 @@ import Product from "./pages/Product";
 import PageNotFound from "./pages/PageNotFound";
 import AppLayout from "./pages/AppLayout";
 import Login from "./pages/Login";
+import CityList from "./components/CityList";
+import CountryList from "./components/CountryList";
+import { useEffect, useState } from "react";
+
+const BASE_URL = "http://localhost:9000";
 
 function App() {
+  const [isloading, setIsloading] = useState(false);
+  const [cities, setCities] = useState([]);
+
+  useEffect(function () {
+    async function fetchCities() {
+      try {
+        setIsloading(true);
+        const res = await fetch(`${BASE_URL}/cities`);
+        const data = await res.json();
+        setCities(data);
+      } catch {
+        alert("Something run in the fetch");
+      } finally {
+        setIsloading(false);
+      }
+    }
+    fetchCities();
+  }, []);
+
   return (
     <div>
       <h3>App</h3>
@@ -15,7 +39,21 @@ function App() {
           <Route path="/" element={<Homepage />} />
           <Route path="/pricing" element={<Pricing />} />
           <Route path="/product" element={<Product />} />
-          <Route path="/app" element={<AppLayout />} />
+          <Route path="/app" element={<AppLayout />}>
+            <Route
+              index
+              element={<CityList cities={cities} isloading={isloading} />}
+            />
+            <Route
+              path="cities"
+              element={<CityList cities={cities} isloading={isloading} />}
+            />
+            <Route
+              path="countries"
+              element={<CountryList cities={cities} isloading={isloading} />}
+            />
+            <Route path="form" element={<p>Form</p>} />
+          </Route>
           <Route path="/login" element={<Login />} />
           <Route path="*" element={<PageNotFound />} />
         </Routes>
---
+++ worldwise/src/components/CountryList.jsx
@@ -0,0 +1,29 @@
+/* eslint-disable react/prop-types */
+import CountryItem from "./CountryItem";
+import styles from "./CountryList.module.css";
+import Spinner from "./Spinner";
+import Message from "./Message";
+
+function CountryList({ cities, isloading }) {
+  if (isloading) return <Spinner />;
+  if (!cities.length) {
+    return (
+      <Message message="Add your first city by clicking on a city on the map" />
+    );
+  }
+  const countries = cities.reduce((arr, city) => {
+    if (!arr.map((el) => el.country).includes(city.country)) {
+      return [...arr, { country: city.country, emoji: city.emoji }];
+    } else return arr;
+  }, []);
+
+  return (
+    <ul className={styles.countryList}>
+      {countries.map((country) => (
+        <CountryItem country={country} key={country} />
+      ))}
+    </ul>
+  );
+}
+
+export default CountryList;
