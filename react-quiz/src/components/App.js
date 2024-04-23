c6ee8d2e7fc06e3c6e9e8669824010cc25b25e89
yikliu <yikliu@umich.edu>
2024-04-22 20:54:44 -0400

    add color css effect to the question / option className and newAnswer useReducer case

--- react-quiz/src/components/App.js
+++ react-quiz/src/components/App.js
@@ -11,6 +11,8 @@ const initialState = {
   // "loading", "ready", "error", "active", "finished"
   status: "loading",
   index: 0,
+  points: 0,
+  answer: null,
 };
 
 function reducer(state, action) {
@@ -21,13 +23,23 @@ function reducer(state, action) {
       return { ...state, status: "error" };
     case "start":
       return { ...state, status: "active" };
+    case "newAnswer":
+      const question = state.questions.at(state.index);
+      return {
+        ...state,
+        answer: action.payload,
+        points:
+          action.payload === question.correctOption
+            ? state.points + question.points
+            : state.points,
+      };
     default:
       throw new Error("Not allowed action type");
   }
 }
 
 export default function App() {
-  const [{ questions, status, index }, dispatch] = useReducer(
+  const [{ questions, status, index, answer }, dispatch] = useReducer(
     reducer,
     initialState
   );
@@ -49,7 +61,13 @@ export default function App() {
         {status === "ready" && (
           <StartScreen numQuestions={numQuestions} dispatch={dispatch} />
         )}
-        {status === "active" && <Question question={questions[index]} />}
+        {status === "active" && (
+          <Question
+            question={questions[index]}
+            dispatch={dispatch}
+            answer={answer}
+          />
+        )}
       </Main>
     </div>
   );
--- react-quiz/src/components/Question.js
+++ react-quiz/src/components/Question.js
@@ -1,10 +1,10 @@
 import Option from "./Option";
 
-function Question({ question }) {
+function Question({ question, dispatch, answer }) {
   return (
     <div>
       <h4>{question.question}</h4>
-      <Option question={question} />
+      <Option question={question} dispatch={dispatch} answer={answer} />
     </div>
   );
 }
